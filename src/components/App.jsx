43749adf57bd4a19122a6cb129433279970f6237
David Whipple <dwhipp88@gmail.com>
2024-01-05 18:04:32 -0700

    updated Note component to use data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,23 @@ import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
 
+import notes from '../notes';
+
 const App = () => {
+    // console.log('Notes: ', notes);
     return (
         <>
             <Header />
-            <Note />
+            {notes.map((note) => (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            ))}
             <Footer />
-            </>
-    )
-}
+        </>
+    );
+};
 
 export default App;
